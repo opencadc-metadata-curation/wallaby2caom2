8823f39f5cafe9d2156c364b0fe934743b8871ec
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-10-18 15:37:12 -0700

    Add default files.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -6,11 +6,16 @@ then
 fi
 if [[ ! -e ${PWD}/state.yml ]]
 then
-  yesterday=$(date -d yesterday "+%d-%b-%Y %H:%M")
-  echo "bookmarks:
-  wallaby_timestamp:
-    last_record: $yesterday
+if [[ ! -e ${PWD}/state.yml ]]; then
+  if [[ "${@}" == "wallaby_run_state" ]]; then
+    yesterday=$(date -d yesterday "+%d-%b-%Y %H:%M")
+    echo "bookmarks:
+    wallaby_timestamp:
+      last_record: $yesterday
 " > ${PWD}/state.yml
+  else
+    cp /usr/local/bin/state.yml ${PWD}
+  fi
 fi
 
 exec "${@}"
