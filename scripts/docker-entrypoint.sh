376a7681d3c40c4518d877f7a963acc431b170fd
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-11-30 17:49:16 -0800

    Remove state.yml handling from docker-entrypoint.sh.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,16 +5,4 @@ then
   cp /usr/local/bin/config.yml ${PWD}
 fi
 
-if [[ ! -e ${PWD}/state.yml ]]; then
-  if [[ "${@}" == "wallaby_run_state" ]]; then
-    yesterday=$(date -d yesterday "+%d-%b-%Y %H:%M")
-    echo "bookmarks:
-    wallaby_timestamp:
-      last_record: $yesterday
-" > ${PWD}/state.yml
-  else
-    cp /usr/local/bin/state.yml ${PWD}
-  fi
-fi
-
 exec "${@}"
